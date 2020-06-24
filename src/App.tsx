301a2d1c8838918d3f6618bac188024bb1c25872
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-23 21:31:59 -0300

    adding view and provider around the app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+
+import { AuthProvider } from './hooks/auth';
 
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e28" />
-    <Routes />
+    <AuthProvider>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </AuthProvider>
   </NavigationContainer>
 );
 
