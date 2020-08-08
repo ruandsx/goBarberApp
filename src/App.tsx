c5ad6820d13169a99b73300413d2d5132753d4f1
Ruan Xavier <ruandsxavier@gmail.com>
2020-08-08 14:45:42 -0300

    adding and setting up react-native-splash-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -8,15 +9,26 @@ import { AuthProvider } from './hooks/auth';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" translucent backgroundColor="#312e28" />
-    <AuthProvider>
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Routes />
-      </View>
-    </AuthProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    // do all things before hid splash screen
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="#312e28"
+      />
+      <AuthProvider>
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </View>
+      </AuthProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
